c90bc6982d8111ea69d8113aef553bbff39566b2
Itsuki Goya <e185716@ie.u-ryukyu.ac.jp>
2018-11-26 16:02:00 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 public class Main {
     public static void main(String[] args){
 
-        String str = null;
+        String str = "百二十三";
 
-        try{
-            System.out.println(str.length());
-
-        }catch(NullPointerException e){
-
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println(e.getMessage());
-
-
-        }
+        int value = Integer.parseInt(str);
 
 
     }
